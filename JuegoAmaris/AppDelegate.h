048455a643dac3b896badf725f34d34c9d4365da
javier <jsantana@melio.tv>
2017-12-19 19:59:25 +0100

    Initial Commit

---
+++ JuegoAmaris/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JuegoAmaris
+//
+//  Created by test on 19/12/17.
+//  Copyright © 2017 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JuegoAmaris/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JuegoAmaris
+//
+//  Created by test on 19/12/17.
+//  Copyright © 2017 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
